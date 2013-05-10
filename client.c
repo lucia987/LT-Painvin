bb4a6b2c04fab6b3427592ecfc1d4d3bcc5eded1
Lucia <lucia@cinnamint-xps.(none)>
2013-05-10 17:20:51 +0300

    Client and server stubs

---
+++ client.c
@@ -0,0 +1,23 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <string.h>
+#include <stdio.h>
+#include "compress.h"
+
+int main()
+{
+	/* Create socket */
+	
+	/* Connect to server socket */
+
+	/* Send message */
+
+	/* Receive confirmation */
+
+	/* Close socket */
+
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,22 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main()
+{
+	/* Create socket */
+
+	/* Bind socket */
+
+	/* Listen on socket */
+
+	/* Accept incoming connection */
+
+	/* Receive message */
+
+	/* Send confirmation */
+
+	/* Close socket */
+	return 0;
+}
